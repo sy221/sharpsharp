27c6d801131c6986c4c84e005226b33a429826ae
tutu1shun <tutu1shun1@yahoo.co.jp>
2015-02-22 00:53:19 +0900

    no message

---
+++ Assets/tutui/controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.UpArrow) )
+		{
+			this.transform.position+= new Vector3 (0,0.3f,0);
+				}
+
+	}
+}
